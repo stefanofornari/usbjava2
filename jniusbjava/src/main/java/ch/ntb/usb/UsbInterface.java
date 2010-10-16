4fc51cfb2bdc1c1a903b49c7fca0e1f1b3fe98cf
Stefano Fornari <stefano.fornari@gmail.com>
2010-10-16 16:30:28 +0200

    added utility methods to aggragate functions in the owned interface descriptors

--- jniusbjava/src/main/java/ch/ntb/usb/UsbInterface.java
+++ jniusbjava/src/main/java/ch/ntb/usb/UsbInterface.java
@@ -7,6 +7,9 @@
  */
 package ch.ntb.usb;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 /**
  * Represents an USB interface.<br>
  * An interface is a group of alternate settings of a configuration.<br>
@@ -61,14 +64,14 @@ public class UsbInterface {
     }
 
     /**
-     * Determines if the given interface supports the given class value
+     * Determines if the given interface belongs to the given class
      *
-     * @param c the class to check
+     * @param c the class to look for
      *
-     * @return true if one of the interface descriptors belongs to the given
-     *         class, false otherwise
+     * @return true if one of the interface belongs to the given class,
+     *         false otherwise
      */
-    public boolean isInClass(byte c) {
+    public boolean belongsToClass(byte c) {
         for (int i=0; i<getNumAlternateSetting(); ++i) {
             if (altsetting[i].getInterfaceClass() == c) {
                 return true;
@@ -76,5 +79,59 @@ public class UsbInterface {
         }
 
         return false;
+    }
+
+    /**
+     * Determines if the given interface belongs to the given sub class
+     *
+     * @param subc the subclass to look for
+     *
+     * @return true if one of the interface belongs to the given sub-class,
+     *         false otherwise
+     */
+    public boolean belongsToSubClass(byte subc) {
+        for (int i=0; i<getNumAlternateSetting(); ++i) {
+            if (altsetting[i].getInterfaceSubClass() == subc) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * Determines if the given interface supports to the given protocol
+     *
+     * @param p the protocol to look for
+     *
+     * @return true if one of the interface supports the given protocol,
+     *         false otherwise
+     */
+    public boolean supportsProtocol(byte p) {
+        for (int i=0; i<getNumAlternateSetting(); ++i) {
+            if (altsetting[i].getInterfaceProtocol() == p) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * Returns all endpoints for all interfaces
+     *
+     * @return all endpoints for all interfaces
+     */
+    public ArrayList<UsbEndpointDescriptor> getAllEndpoints() {
+        ArrayList<UsbEndpointDescriptor> ret =
+            new ArrayList<UsbEndpointDescriptor>();
+
+        if (altsetting != null) {
+            for(UsbInterfaceDescriptor d: altsetting) {
+                Collections.addAll(ret, d.getEndpoints());
+            }
+        }
+
+        return ret;
     }
 }
--- jniusbjava/src/test/java/ch/ntb/usb/UsbInterfaceTest.java
+++ jniusbjava/src/test/java/ch/ntb/usb/UsbInterfaceTest.java
@@ -5,6 +5,7 @@
 
 package ch.ntb.usb;
 
+import java.util.ArrayList;
 import junit.framework.TestCase;
 
 /**
@@ -30,8 +31,23 @@ public class UsbInterfaceTest extends TestCase {
             new UsbInterfaceDescriptor()
         };
 
+        UsbEndpointDescriptor[] endpoints1 = new UsbEndpointDescriptor[] {
+            new UsbEndpointDescriptor(),
+            new UsbEndpointDescriptor(),
+        };
+
+        UsbEndpointDescriptor[] endpoints2 = new UsbEndpointDescriptor[] {
+            new UsbEndpointDescriptor()
+        };
+
         desc[0].setInterfaceClass((byte)0x01);
         desc[1].setInterfaceClass((byte)0x02);
+        desc[0].setInterfaceSubClass((byte)0x3);
+        desc[1].setInterfaceSubClass((byte)0x4);
+        desc[0].setInterfaceProtocol((byte)0x05);
+        desc[1].setInterfaceProtocol((byte)0x06);
+        desc[0].setEndpoints(endpoints1);
+        desc[1].setEndpoints(endpoints2);
 
         INTERFACE.setAlternateSetting(desc);
     }
@@ -52,12 +68,48 @@ public class UsbInterfaceTest extends TestCase {
         assertEquals(2, INTERFACE.getNumAlternateSetting());
     }
 
-    public void testIsNotClass() {
-        assertFalse(INTERFACE.isInClass((byte)0x06));
+    public void testDoesNotBelongToClass() {
+        assertFalse(INTERFACE.belongsToClass((byte)0x06));
     }
     
-    public void testIsClass() {
-        assertTrue(INTERFACE.isInClass((byte)0x01));
-        assertTrue(INTERFACE.isInClass((byte)0x02));
+    public void testBelongsToClass() {
+        assertTrue(INTERFACE.belongsToClass((byte)0x01));
+        assertTrue(INTERFACE.belongsToClass((byte)0x02));
     }
+
+    public void testDoesNotBelongToSubClass() {
+        assertFalse(INTERFACE.belongsToSubClass((byte)0x06));
+    }
+
+    public void testBelongsToSubClass() {
+        assertTrue(INTERFACE.belongsToSubClass((byte)0x03));
+        assertTrue(INTERFACE.belongsToSubClass((byte)0x04));
+    }
+
+    public void testDoesNotSupportProtocol() {
+        assertFalse(INTERFACE.supportsProtocol((byte)0xff));
+    }
+
+    public void testSupportsProtocol() {
+        assertTrue(INTERFACE.supportsProtocol((byte)0x05));
+        assertTrue(INTERFACE.supportsProtocol((byte)0x06));
+    }
+
+    public void testNotNullEndpoints() {
+        ArrayList endpoints = new UsbInterface().getAllEndpoints();
+
+        assertNotNull(endpoints);
+    }
+
+    public void testGetAllEndpointsEmpty() {
+        ArrayList endpoints = new UsbInterface().getAllEndpoints();
+
+        assertEquals(0, endpoints.size());
+    }
+    
+    public void testGetAllEndpointsFull() {
+        assertEquals(3, INTERFACE.getAllEndpoints().size());
+    }
+
+
 }
