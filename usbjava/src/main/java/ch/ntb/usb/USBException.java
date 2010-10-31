aac4fa06c14d464a9fa7631b2b0e307c234d9293
Stefano Fornari <stefano.fornari@gmail.com>
2010-10-31 18:37:05 +0100

    Added isStalled()

--- usbjava/src/main/java/ch/ntb/usb/USBException.java
+++ usbjava/src/main/java/ch/ntb/usb/USBException.java
@@ -1,6 +1,6 @@
 /* 
  * Java libusb wrapper
- * Copyright (c) 2005-2006 Andreas Schläpfer <spandi at users.sourceforge.net>
+ * Copyright (c) 2005-2006 Andreas Schlï¿½pfer <spandi at users.sourceforge.net>
  *
  * http://libusbjava.sourceforge.net
  * This library is covered by the LGPL, read LGPL.txt for details.
@@ -11,13 +11,22 @@ import java.io.IOException;
 
 public class USBException extends IOException {
 
-	public USBException(String string) {
-		super(string);
-	}
+    public USBException(String string) {
+        super(string);
+    }
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1690857437804284710L;
+    /**
+     * Returns true iff the exception indicates a (bulk) endpoint has
+     * stalled; these are used as error indicators in device protocols.
+     *
+     * For now this is just imported from jusb with a default implementation.
+     */
+    public boolean isStalled() {
+        return false;
+    };
 
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1690857437804284710L;
 }
--- usbjava/src/main/java/ch/ntb/usb/USBTimeoutException.java
+++ usbjava/src/main/java/ch/ntb/usb/USBTimeoutException.java
@@ -1,6 +1,6 @@
 /* 
  * Java libusb wrapper
- * Copyright (c) 2005-2006 Andreas Schläpfer <spandi at users.sourceforge.net>
+ * Copyright (c) 2005-2006 Andreas Schlï¿½pfer <spandi at users.sourceforge.net>
  *
  * http://libusbjava.sourceforge.net
  * This library is covered by the LGPL, read LGPL.txt for details.
@@ -9,13 +9,24 @@ package ch.ntb.usb;
 
 public class USBTimeoutException extends USBException {
 
-	public USBTimeoutException(String string) {
-		super(string);
-	}
+    public USBTimeoutException(String string) {
+        super(string);
+    }
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = -1065328371159778249L;
+    /**
+     * Returns true iff the exception indicates a (bulk) endpoint has
+     * stalled; these are used as error indicators in device protocols.
+     *
+     * For now this is just imported from jusb with a default implementation.
+     */
+    @Override
+    public boolean isStalled() {
+        return true;
+    }
 
+    ;
+    /**
+     *
+     */
+    private static final long serialVersionUID = -1065328371159778249L;
 }
