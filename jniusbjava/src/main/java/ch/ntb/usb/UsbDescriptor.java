ef12e9618ffdbb150d8e5adbcb9340125f247075
Stefano Fornari <stefano.fornari@gmail.com>
2010-10-31 18:30:27 +0100

    Reformatted constants declaration

--- jniusbjava/src/main/java/ch/ntb/usb/UsbDescriptor.java
+++ jniusbjava/src/main/java/ch/ntb/usb/UsbDescriptor.java
@@ -16,23 +16,29 @@ public class UsbDescriptor {
 	/**
 	 * Descriptor types ({@link #bDescriptorType}).
 	 */
-	public static final int USB_DT_DEVICE = 0x01, USB_DT_CONFIG = 0x02,
-			USB_DT_STRING = 0x03, USB_DT_INTERFACE = 0x04,
-			USB_DT_ENDPOINT = 0x05;
+	public static final int USB_DT_DEVICE    = 0x01,
+                                USB_DT_CONFIG    = 0x02,
+			        USB_DT_STRING    = 0x03,
+                                USB_DT_INTERFACE = 0x04,
+			        USB_DT_ENDPOINT  = 0x05;
 
 	/**
 	 * Descriptor types ({@link #bDescriptorType}).
 	 */
-	public static final int USB_DT_HID = 0x21, USB_DT_REPORT = 0x22,
-			USB_DT_PHYSICAL = 0x23, USB_DT_HUB = 0x29;
+	public static final int USB_DT_HID      = 0x21,
+                                USB_DT_REPORT   = 0x22,
+			        USB_DT_PHYSICAL = 0x23,
+                                USB_DT_HUB      = 0x29;
 
 	/**
 	 * Descriptor sizes per descriptor type ({@link #bLength}).
 	 */
-	public static final int USB_DT_DEVICE_SIZE = 18, USB_DT_CONFIG_SIZE = 9,
-			USB_DT_INTERFACE_SIZE = 9, USB_DT_ENDPOINT_SIZE = 7,
-			USB_DT_ENDPOINT_AUDIO_SIZE = 9 /* Audio extension */,
-			USB_DT_HUB_NONVAR_SIZE = 7;
+	public static final int USB_DT_DEVICE_SIZE = 18,
+                                USB_DT_CONFIG_SIZE = 9,
+			        USB_DT_INTERFACE_SIZE = 9,
+                                USB_DT_ENDPOINT_SIZE = 7,
+			        USB_DT_ENDPOINT_AUDIO_SIZE = 9 /* Audio extension */,
+			        USB_DT_HUB_NONVAR_SIZE = 7;
 
 	private byte bLength;
 
